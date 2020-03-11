9cab94d6b0252cb5ef65cd094bb226272da77655
Игорь Тихоненко <monzar1993@outlook.com>
2020-03-11 19:09:57 +0200

    responsive start

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+// navbar
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+// navbar
+
+//tabs
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
 
@@ -14,3 +26,31 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+//tabs
+
+
+//timer
+// для задания обратного отсчета до определенной даты укажите дату в формате:
+// timeend= new Date(ГОД, МЕСЯЦ-1, ДЕНЬ);
+// Для задания даты с точностью до времени укажите дату в формате:
+// timeend= new Date(ГОД, МЕСЯЦ-1, ДЕНЬ, ЧАСЫ-1, МИНУТЫ);
+
+timeend = new Date();
+timeend = new Date(timeend.getYear() > 1900 ? (timeend.getYear() + 1) : (timeend.getYear() + 1900), 2, 15);
+
+function time() {
+    today = new Date();
+    today = Math.floor((timeend - today) / 1000);
+    tsec = today % 60;
+    today = Math.floor(today / 60);
+    if (tsec < 10) tsec = '0' + tsec;
+    tmin = today % 60;
+    today = Math.floor(today / 60);
+    if (tmin < 10) tmin = '0' + tmin;
+    thour = today % 24;
+    today = Math.floor(today / 24);
+    timestr = today + ":" + thour + ":" + tmin + ":" + tsec;
+    document.getElementById('t').innerHTML = timestr;
+    window.setTimeout("time()", 1000);
+}
+//timer
