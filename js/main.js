bb3011a9fd360549e9df1b179f6ec334609a17fb
Игорь Тихоненко <monzar1993@outlook.com>
2020-03-12 11:30:18 +0200

    responsive menu

--- js/main.js
+++ js/main.js
@@ -7,6 +7,15 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById("logo").style.width = "45px";
+    } else {
+        document.getElementById("logo").style.width = "160px";
+    }
+}
 // navbar
 
 //tabs
